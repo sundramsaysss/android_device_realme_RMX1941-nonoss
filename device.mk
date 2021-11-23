5fcfde4a19541c99157075d52747835de6295044
LuK1337 <priv.luk@gmail.com>
2021-11-23 17:00:56 +0530

    RMX1941: Build libhidltransport/libhwbinder * Needed for old blobs
    Signed-off-by: cArN4gEisDeD <carn4geisd3d@gmail.com>

--- device.mk
+++ device.mk
@@ -64,7 +64,11 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0_system \
-    android.hidl.manager@1.0_system 
+    android.hidl.manager@1.0_system \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # Symbols
 PRODUCT_PACKAGES += \
