6a95b8f4ab59ebd548746259d66b0704da5571cc
SparXFusion <s2234nadar@gmail.com>
2021-11-23 16:54:08 +0530

    RMX1941: Install GSI keys to ramdisk
    Signed-off-by: cArN4gEisDeD <carn4geisd3d@gmail.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@
 
 DEVICE_PATH := device/realme/RMX1941
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay 
